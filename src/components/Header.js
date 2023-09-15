ce2be0665ba8963acab2e2c69a77a795e35cebc3
Sachin Heshan Munasinghe <heshanf45@gmail.com>
2023-09-15 19:54:14 +0530

    add style link in to index html

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,6 @@ function Header(){
             <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
             <title>Modernize Free</title>
             <link rel="shortcut icon" type="image/png" href="../assets/images/logos/favicon.png" />
-            <link rel="stylesheet" href="../assets/css/styles.min.css" />
         </head>
     )
 
