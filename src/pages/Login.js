78e31e4f2e010b58c6e1c7dcdde44de7bcb784e7
Sachin Heshan Munasinghe <heshanf45@gmail.com>
2023-09-29 19:06:42 +0530

    add form validation using formik

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -5,6 +5,7 @@ import { useState, useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
 import {toast} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 
 
@@ -13,7 +14,7 @@ function Login(){
     const [password, setPassword] = useState("");
     const navigate = useNavigate();
 
-    async function login(event) {
+    async function onSubmit(event) {
       event.preventDefault();
       try {
         await axios.post("http://localhost:8080/api/v1/employee/login", {
@@ -51,55 +52,80 @@ function Login(){
     return(
         <React.Fragment>
            <Header />
-           <div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full" data-sidebar-position="fixed" data-header-position="fixed">
-            <div
-              class="position-relative overflow-hidden radial-gradient min-vh-100 d-flex align-items-center justify-content-center">
-              <div class="d-flex align-items-center justify-content-center w-100">
-                <div class="row justify-content-center w-100">
-                  <div class="col-md-8 col-lg-6 col-xxl-3">
-                    <div class="card mb-0">
-                      <div class="card-body">
-                        <a href="./index.html" class="text-nowrap logo-img text-center d-block py-3 w-100">
-                          <img src="../assets/images/logos/dark-logo.svg" width="180" alt=""></img>
-                        </a>
-                        <p class="text-center"></p>
-                        <form>
-                          <div class="mb-3">
-                            <label for="exampleInputEmail1" class="form-label">Username</label>
-                            <input type="email" class="form-control" id="email" aria-describedby="emailHelp"value={email}
-                            onChange={(event) => {
-                              setEmail(event.target.value);
-                            }}></input>
-                          </div>
-                          <div class="mb-4">
-                            <label for="exampleInputPassword1" class="form-label">Password</label>
-                            <input type="password" class="form-control" id="password"value={password}
-                            onChange={(event) => {
-                              setPassword(event.target.value);
-                            }}></input>
-                          </div>
-                          <div class="d-flex align-items-center justify-content-between mb-4">
-                            <div class="form-check">
-                              <input class="form-check-input primary" type="checkbox" value="" id="flexCheckChecked" checked></input>
-                              <label class="form-check-label text-dark" for="flexCheckChecked">
-                                Remeber this Device
-                              </label>
+            <div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full" data-sidebar-position="fixed" data-header-position="fixed">
+                  <Formik
+                    initialValues={{ email: '', password: '' }}
+                    validate={values => {
+                      const errors = {};
+                      if (!values.email) {
+                        errors.email = 'Required';
+                      } else if (
+                        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+                      ) {
+                        errors.email = 'Invalid email address';
+                      }
+                      return errors;
+                    }}
+                    onSubmit={(values, { setSubmitting }) => {
+                      setTimeout(() => {
+                        alert(JSON.stringify(values, null, 2));
+                        setSubmitting(false);
+                      }, 400);
+                    }}
+                    >
+                  {({ isSubmitting }) => (
+                    <div class="position-relative overflow-hidden radial-gradient min-vh-100 d-flex align-items-center justify-content-center">
+                      <div class="d-flex align-items-center justify-content-center w-100">
+                        <div class="row justify-content-center w-100">
+                          <div class="col-md-8 col-lg-6 col-xxl-3">
+                            <div class="card mb-0">
+                              <div class="card-body">
+                                <a href="./index.html" class="text-nowrap logo-img text-center d-block py-3 w-100">
+                                  <img src="../assets/images/logos/dark-logo.svg" width="180" alt=""></img>
+                                </a>
+                                <p class="text-center"></p>
+                                <form>
+                                  <div class="mb-3">
+                                    <label for="exampleInputEmail1" class="form-label">Username</label>
+                                    <Field type="email" class="form-control" id="email" aria-describedby="emailHelp"value={email}
+
+                                    onChange={(event) => {
+                                      setEmail(event.target.value);
+                                    }}></Field>
+                                    <ErrorMessage name="email" component="div" />
+                                  </div>
+                                  <div class="mb-4">
+                                    <label for="exampleInputPassword1" class="form-label">Password</label>
+                                    <Field type="password" class="form-control" id="password"value={password}
+                                    onChange={(event) => {
+                                      setPassword(event.target.value);
+                                    }}></Field>
+                                    <ErrorMessage name="password" component="div" />
+                                  </div>
+                                  <div class="d-flex align-items-center justify-content-between mb-4">
+                                    <div class="form-check">
+                                      <input class="form-check-input primary" type="checkbox" value="" id="flexCheckChecked" checked></input>
+                                      <label class="form-check-label text-dark" for="flexCheckChecked">
+                                        Remeber this Device
+                                      </label>
+                                    </div>
+                                    <a class="text-primary fw-bold" href="./index.html">Forgot Password ?</a>
+                                  </div>
+                                  <a class="btn btn-primary w-100 py-8 fs-4 mb-4 rounded-2" onClick={onSubmit} disabled={isSubmitting}>Sign In</a>
+                                  <div class="d-flex align-items-center justify-content-center">
+                                      {/* <p class="fs-4 mb-0 fw-bold">New to Modernize?</p> */}
+                                    {/* <a class="text-primary fw-bold ms-2" href="./authentication-register.html">Create an account</a> */}
+                                  </div>
+                                </form>
+                              </div>
                             </div>
-                            <a class="text-primary fw-bold" href="./index.html">Forgot Password ?</a>
                           </div>
-                          <a class="btn btn-primary w-100 py-8 fs-4 mb-4 rounded-2" onClick={login}>Sign In</a>
-                          <div class="d-flex align-items-center justify-content-center">
-                            {/* <p class="fs-4 mb-0 fw-bold">New to Modernize?</p> */}
-                            {/* <a class="text-primary fw-bold ms-2" href="./authentication-register.html">Create an account</a> */}
-                          </div>
-                        </form>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                </div>
-              </div>
+                  )}
+                </Formik>
             </div>
-          </div>
           <script src="../assets/libs/jquery/dist/jquery.min.js"></script>
           <script src="../assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
       </React.Fragment>
