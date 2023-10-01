10c87f2bb5e926232531281a9c42b848a61d8a2f
Sachin Heshan Munasinghe <heshanf45@gmail.com>
2023-10-01 12:38:16 +0530

    add user add form

--- src/pages/User-add.js
+++ src/pages/User-add.js
@@ -8,9 +8,31 @@ import Nav from "../components/Navigation";
 import 'react-toastify/dist/ReactToastify.css';
 
 
+function Useradd(){
+    const [name, setName] = useState("");
+    const [phonenumber, setPhoneNumber] = useState("");
+    const [fedexid, setFedexID] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
+    const onSubmit = async () => {
+        axios.post('http://localhost:8080/api/v1/employee/saveEmployee', {
+            name: name,
+            phonenumber: phonenumber,
+            fedexid: fedexid,
+            email: email,
+            password: password
+          })
+          .then(function (response) {
+            toast.success('Data Added',
+                      {position: toast.POSITION.TOP_RIGHT})
+          })
+          .catch(function (error) {
+            toast.success('Somthing occured while data saving',
+                {position: toast.POSITION.TOP_RIGHT})
+          });
+    }
 
-function Login(){
     return(
         <React.Fragment>
            <Header />
@@ -23,34 +45,73 @@ function Login(){
                             <div className="row">
                                 <div className="form-group col-md-6">
                                     <label htmlFor="inputEmail4">Name</label>
-                                    <input type="text" className="form-control" id="inputEmail4" placeholder="Name"/>
+                                    <input type="text" className="form-control" id="inputEmail4" placeholder="Name" valu={name}
+                                    onChange={(event) => {
+                                      setName(event.target.value);
+                                    }}/>
                                 </div>
                                 <div className="form-group col-md-6">
                                     <label htmlFor="inputPassword4">Phone Number</label>
-                                    <input type="text" className="form-control" id="inputPassword4" placeholder="Phone Number"/>
+                                    <input type="text" className="form-control" id="inputPassword4" placeholder="Phone Number" value={phonenumber}
+                                    onChange={(event) => {
+                                        setPhoneNumber(event.target.value);
+                                      }}/>
                                 </div>
                             </div>
                             <div className="row">
                                 <div className="form-group col-md-6">
                                     <label htmlFor="inputEmail4">Fedex ID</label>
-                                    <input type="text" className="form-control" id="inputEmail4" placeholder="Fedex ID"/>
+                                    <input type="text" className="form-control" id="inputEmail4" placeholder="Fedex ID" value={fedexid}
+                                        onChange={(event) => {
+                                            setFedexID(event.target.value);
+                                    }}/>
                                 </div>
                                 <div className="form-group col-md-6">
                                     <label htmlFor="inputPassword4">Email</label>
-                                    <input type="text" className="form-control" id="inputPassword4" placeholder="Email"/>
+                                    <input type="text" className="form-control" id="inputPassword4" placeholder="Email" value={email}
+                                        onChange={(event) => {
+                                            setEmail(event.target.value);
+                                    }}/>
                                 </div>
                             </div>
                             <div className="row">
                                 <div className="form-group col-md-6">
                                     <label htmlFor="inputPassword4">Password</label>
-                                    <input type="text" className="form-control" id="inputPassword4" placeholder="Password"/>
+                                    <input type="text" className="form-control" id="inputPassword4" placeholder="Password" value={password}
+                                        onChange={(event) => {
+                                            setPassword(event.target.value);
+                                    }}/>
                                 </div>
                             </div>
                             <br></br>
-                            <button type="submit" className="btn btn-primary">Submit</button>
+                            <button type="submit" className="btn btn-primary" onClick={onSubmit}>Submit</button>
                             </form>
                         </div>
                     </div>
+                </div>
+                <div className="body-wrapper">
+                    <div className="container-fluid">
+                        <div className="box form-box">
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">First</th>
+                            <th scope="col">Last</th>
+                            <th scope="col">Handle</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                </div>
                 </div>
             </div>
             <script src="../assets/libs/jquery/dist/jquery.min.js"></script>
@@ -65,4 +126,4 @@ function Login(){
     )
 
 }
-export default Login;
+export default Useradd;
